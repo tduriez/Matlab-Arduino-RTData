2975394b960d5fc1334c9b9dc0784d29cd405bc3
Thomas Duriez <thomas.duriez@gmail.com>
2017-09-08 13:35:24 -0300

    continued editing class

--- RTDataHardware.m
+++ RTDataHardware.m
@@ -4,16 +4,42 @@ classdef RTDataHardware < handle
     %arduino > Arduino
     %delay > Delay
     
-   properties
-   arduino
-   delay
-   Port
-   Channels
-   nMeasures
-   Bits
-   Volts
-   Serial
+   properties (SetObservable)
+      arduino='due'
+      delay=1000
+      Port
+      Channels=2
+      nMeasures=1
    end
+   
+   properties (Hidden,SetAccess=private)
+      Bits=12
+      Volts=3.3
+   end
+   
+   properties (SetAccess=private)
+      Serial
+   end
+   
+   methods
+       function obj=RTDataHardware(ThePort);
+           obj.Port=ThePort;
+       end
+       
+       function openPort(obj)
+           
+       end
+       
+       function closePort(obj)
+       
+       end
+       
+       function delete(obj)
+           
+       end
+       
+   end
+   
    
    
 end
