61d3ea4b482496f13a307e923ad4d1b1ab20e2d3
Thomas Duriez <thomas.duriez@gmail.com>
2017-09-29 12:56:36 -0300

    RTDataHardware/disp is now a separate file

--- @RTDataHardware/RTDataHardware.m
+++ @RTDataHardware/RTDataHardware.m
@@ -59,39 +59,8 @@ classdef RTDataHardware < matlab.mixin.Copyable
         end
         
         %% Display overload
-        function disp(obj,mode)
-            if nargin<2
-                mode='all';
-            end
-            disptxt=[];
-            if strcmpi(mode,'all')
-                disptxt=sprintf('RTData Hardware Object:\n\n');
-            end
-            if any(strcmpi(mode,{'serial','all','simple'}))
-                disptxt=sprintf('%s -Serial configuration:\n',disptxt);
-                disptxt=sprintf('%s    -Serial Port: %s\n',disptxt,obj.Port);
-                if isa(obj.Serial,'serial')
-                    disptxt=sprintf('%s    -Baudrate:    %d\n',disptxt,get(obj.Serial,'BaudRate'));
-                    disptxt=sprintf('%s    -Buffer size: %d\n',disptxt,get(obj.Serial,'InputBufferSize'));
-                    disptxt=sprintf('%s    -Status:      %s\n',disptxt,get(obj.Serial,'Status'));
-                else
-                    disptxt=sprintf('%s    -Status:      %s\n',disptxt,'not created');
-                end
-            end
-            if strcmpi(mode,'all')
-                disptxt=sprintf('%s\n',disptxt);
-            end
-            if any(strcmpi(mode,{'arduino','all','simple'}))
-                disptxt=sprintf('%s -Arduino configuration:\n',disptxt);
-                disptxt=sprintf('%s    -Type:        %s\n',disptxt,obj.Arduino);
-                disptxt=sprintf('%s         -Bits:   %d\n',disptxt,obj.Bits);
-                disptxt=sprintf('%s         -Volts:  %.1f\n',disptxt,obj.Volts);
-                disptxt=sprintf('%s    -Delay:       %d us (%.2f Hz)\n',disptxt,obj.Delay,10^6/obj.Delay);
-                disptxt=sprintf('%s    -Channels:    %d\n',disptxt,obj.Channels);
-                disptxt=sprintf('%s    -Measures:    %d\n',disptxt,obj.nMeasures);
-            end
-            fprintf(disptxt);
-        end
+        disp(obj,mode)
+        
     end
     
      methods (Static, Hidden)  
